5be46ba3d00c5f6c06a4b005cfb3e76cffdb6475
Poire Molle <s224311@dtu.dk>
2023-04-04 12:37:59 +0200

    Adding a CHeckpoint class that extends BoardElement

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoint.java
@@ -0,0 +1,11 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+public class Checkpoint extends BoardElement {
+    private Space locationSpace;
+
+    public Checkpoint(Space locationSpace) {
+        this.locationSpace = locationSpace;
+    }
+
+
+}
