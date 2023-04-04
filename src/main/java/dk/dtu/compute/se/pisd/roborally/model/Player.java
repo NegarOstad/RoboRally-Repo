401884658f2fc9696f9a90ed9b1dbcf23eca1e8c
Poire Molle <s224311@dtu.dk>
2023-04-04 12:28:54 +0200

    Adding a checkpoint token count variable and a method to increment it

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -48,6 +48,8 @@ public class Player extends Subject {
     private CommandCardField[] program;
     private CommandCardField[] cards;
 
+    private int checkpointTokenCount = 0;
+
     public Player(@NotNull Board board, String color, @NotNull String name) {
         this.board = board;
         this.name = name;
@@ -133,4 +135,7 @@ public class Player extends Subject {
         return cards[i];
     }
 
+    public void addCheckpointToken() {
+        checkpointTokenCount++;
+    }
 }
