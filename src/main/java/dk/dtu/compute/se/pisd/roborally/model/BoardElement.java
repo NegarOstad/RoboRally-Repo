28a24dde722a38167bd9ad40c98b7acb25751bb5
Poire Molle <s224311@dtu.dk>
2023-04-04 16:27:52 +0200

    Adding a getType method that returns the time of board element it is

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
@@ -1,7 +1,10 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 public abstract class BoardElement {
-    BoardElement() {
-
+    Space spaceLocation;
+    BoardElement(Space spaceLocation) {
+        this.spaceLocation = spaceLocation;
     }
+
+    public abstract String getType();
 }
