3db783eaba036daa12fa479878f98a1dce2c28af
Poire Molle <s224311@dtu.dk>
2023-04-04 16:47:15 +0200

    Adding a setIndex method

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoint.java
@@ -1,8 +1,11 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 public class Checkpoint extends BoardElement {
+
+    int index;
     public Checkpoint(Space spaceLocation) {
         super(spaceLocation);
+        spaceLocation.setBoardElement(this);
 
     }
 
@@ -10,5 +13,8 @@ public class Checkpoint extends BoardElement {
         return "Checkpoint";
     }
 
+    public void setIndex(int i) {
+        index = i;
+    }
 
 }
