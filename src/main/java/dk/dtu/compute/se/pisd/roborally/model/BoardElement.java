8aebf86ec3947c0c56849e8a0ffb53e1c448f837
Mirzy12 <s205821@student.dtu.dk>
2023-04-10 13:57:39 +0200

    Creation of method for calculating the closest player by using the Pythagorean theorem to calculate the distance and in turn will give us the player that is closest.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
@@ -5,12 +5,25 @@ public abstract class BoardElement {
 
     private int[] priorityAntennaCoordinates;
 
+    // Creates a list called closestPlayer where the players are listed in ascending order which player has shortest distance to the priority antenna
     public int closestPlayer(List<Player> players) {
-        if (players.isEmpty()) {
-            throw new IllegalArgumentException("Player list is empty");
+
+        // Will loop through each player in the list
+        for (Player player : players) {
+            // Get the coordinates of the player
+            int[] playerCoordinates = player.getCoordinates();
+
+            // the distance between the player and the priority antenna is calculated using the Pythagorean theorem
+            double distance = Math.sqrt(Math.pow(playerCoordinates[0] - priorityAntennaCoordinates[0], 2)
+                    + Math.pow(playerCoordinates[1] - priorityAntennaCoordinates[1], 2));
+
+            // Will update the minimum distance and closest player ID if this player is closer than the previous closest player
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPlayerID = player.getID();
+            }
         }
-
-
-        return 0;
+        //returns the closest player
+        return closestPlayerID;
     }
 }
