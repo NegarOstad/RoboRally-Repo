a3af329493b2b326511e24d5ac4af576d9cc0eea
Mirzy12 <s205821@student.dtu.dk>
2023-04-10 13:39:12 +0200

    Created a method called closestPlayer and it takes a list of players and returns the ID of the player that is the closest to the priority antenna.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
@@ -1,8 +1,16 @@
 package dk.dtu.compute.se.pisd.roborally.model;
+import java.util.List;
 
 public abstract class BoardElement {
 
     private int[] priorityAntennaCoordinates;
 
+    public int closestPlayer(List<Player> players) {
+        if (players.isEmpty()) {
+            throw new IllegalArgumentException("Player list is empty");
+        }
 
+
+        return 0;
+    }
 }
