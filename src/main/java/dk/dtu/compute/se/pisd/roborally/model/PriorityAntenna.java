022416395bb1c638d77a095787e9db1218689776
Mirzy12 <s205821@student.dtu.dk>
2023-04-13 10:32:11 +0200

    Adding elements in PriorityAntenna

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/PriorityAntenna.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/PriorityAntenna.java
@@ -1,5 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
-public class PriorityAntenna extends BoardElement{
+public class PriorityAntenna extends BoardElement {
+    public PriorityAntenna(Space spaceLocation) {
+        super(spaceLocation);
+        spaceLocation.setBoardElement(this);
 
+    }
 }
