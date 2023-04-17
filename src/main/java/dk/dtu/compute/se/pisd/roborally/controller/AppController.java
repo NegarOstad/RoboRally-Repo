58df6fa30901ec43fe6fb5439c9e70bfb69a5f00
Poire Molle <s224311@dtu.dk>
2023-04-17 22:32:00 +0200

    Show get token count on screen

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -91,8 +91,9 @@ public class AppController implements Observer {
             board.getSpace(4,4).setTypeGear(Heading.EAST);
             board.getSpace(1,3).setTypeGear(Heading.SOUTH);
             board.getSpace(4,0).setTypeCheckpoint(0);
-            board.getSpace(6,3).setTypeCheckpoint(1);
-            board.getSpace(1,5).setTypeCheckpoint(2);
+            board.getSpace(5,0).setTypeCheckpoint(1);
+            //board.getSpace(6,3).setTypeCheckpoint(1);
+            //board.getSpace(1,5).setTypeCheckpoint(2);
             board.getSpace(1,1).setTypeConveyor(board.getSpace(6,1));
             board.getSpace(1,6).setTypeConveyor(board.getSpace(3,3));
             //add priority antenna and walls
