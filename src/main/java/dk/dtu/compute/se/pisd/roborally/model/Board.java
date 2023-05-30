57b8f1b85d7f27d7507ac52d31c62b3ea9b5372b
Poire Molle <s224311@dtu.dk>
2023-05-30 20:11:18 +0200

    removing print outs of each Space coordinates

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -67,7 +67,6 @@ public class Board extends Subject {
             for(int y = 0; y < height; y++) {
                 Space space = new Space(this, x, y);
                 spaces[x][y] = space;
-                System.out.println("("+x+", "+y+")");
             }
         }
         this.stepMode = false;
