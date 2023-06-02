8afa6bf67533bb3c440cceed23e3ef05a85116f6
Poire Molle <s224311@dtu.dk>
2023-06-02 03:52:04 +0200

    ADding BoardTemplate class

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardTemplate.java
@@ -0,0 +1,55 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
+import java.util.ArrayList;
+import java.util.List;
+
+import static dk.dtu.compute.se.pisd.roborally.model.Phase.INITIALISATION;
+
+public class BoardTemplate extends Subject {
+
+    public int width;
+    public int height;
+
+
+    public SpaceTemplate[][] spaceTemplates;
+
+    public List<PlayerTemplate> playerTemplates = new ArrayList<>();
+
+    public PlayerTemplate currentTemplate;
+
+
+    public BoardTemplate(int width, int height, Space[][] spaces) {
+        this.width = width;
+        this.height = height;
+        spaceTemplates = new SpaceTemplate[width][height];
+        for (int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                ElementType type = spaces[x][y].getType();
+                //BoardElement be = spaces[x][y].getBoardElement();
+                SpaceTemplate spaceTemplate = new SpaceTemplate(x, y, type/*, be*/);
+                spaceTemplates[x][y] = spaceTemplate;
+            }
+        }
+
+    }
+
+    public void setPlayerTemplates(List<Player> players){
+
+        for(Player p : players){
+            int spaceX = p.getSpace().x;
+            int spaceY = p.getSpace().y;
+            PlayerTemplate newPlayer = new PlayerTemplate(p.getName(), p.getColor(), spaceTemplates[spaceX][spaceY], p.getHeading(), p.getTokenCount(), p.getRegisterStatus());
+            playerTemplates.add(newPlayer);
+        }
+    }
+
+    public void setCurrentPlayerTemplate(Player current) {
+        for (int i = 0; i < playerTemplates.size(); i++) {
+            if(playerTemplates.get(i).name.equals(current.getName())){
+                currentTemplate = playerTemplates.get(i);
+            }
+        }
+    }
+
+}
