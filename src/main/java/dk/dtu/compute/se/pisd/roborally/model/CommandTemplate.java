d4d925cb7fa6ca74e349f605a1cffb62a8bb94b0
Bayan <113907779+BayanDowairi@users.noreply.github.com>
2023-06-04 12:23:54 +0200

    cardfield view problem after removing player from the structure of CommandCardField.java

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/CommandTemplate.java
@@ -0,0 +1,11 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+public class CommandTemplate {
+
+    public Player player;
+
+    private CommandCard card;
+
+    private boolean visible;
+
+    }
