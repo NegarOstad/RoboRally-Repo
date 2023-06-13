2735fe47b176f73cc7efea361ccae03bfd588165
NegarOstad <s224283@student.dtu.dk>
2023-06-13 11:06:37 +0200

    RESET TO A COMMIT

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/api/HTTPClient.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/api/HTTPClient.java
@@ -17,7 +17,7 @@ public class HTTPClient {
     //private final String URL = "http://localhost:8080/";
     //private final String URL = "http://10.209.204.5:8080/";
     //private final String URL = "http://192.168.1.57:8080/";
-    private final String URL = "http://10.209.212.0:8080/";
+    private final String URL = "http://10.209.205.30:8080/";
 
     // Private constructor to prevent instantiation from outside the class
     private HTTPClient() {
