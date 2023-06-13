9190dd15c0728186e625de5c92d275077380f1a0
NegarOstad <s224283@student.dtu.dk>
2023-06-13 09:50:37 +0200

    AppController class diagram

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -54,13 +54,13 @@ public class AppController implements Observer {
 
     final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
     final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
-    final private List<Integer> BOARD_NUMBER = Arrays.asList(1,2);
-    final private List<String> COUNTINUE_OR_NOT = Arrays.asList("Yes" , "N0");
+    //final private List<Integer> BOARD_NUMBER = Arrays.asList(1,2);
+    //final private List<String> COUNTINUE_OR_NOT = Arrays.asList("Yes" , "N0");
     private String[] gameFiles;
     private String gameName ;
     private int gameId;
 
-    Repository repository = Repository.getInstance();
+    private Repository repository = Repository.getInstance();
 
     private boolean isGameSaved = false;
 
@@ -68,11 +68,11 @@ public class AppController implements Observer {
 
     private GameController gameController;
 
-    int playerCount;
+    private int playerCount;
 
-    int playerNum;
+   private int playerNum;
 
-    int numberOfPlayersJoined;
+    private int numberOfPlayersJoined;
     public AppController(@NotNull RoboRally roboRally) {
         this.roboRally = roboRally;
     }
